bf51afcdaf15d70eb8e672633640c7ffad5b045d
hanmoongoo <hanmoongoo@gmail.com>
2021-01-07 02:12:23 +0900

    https 관련 cdn 수정

--- public/htmlSource/lsmall/js/index.js
+++ public/htmlSource/lsmall/js/index.js
@@ -1,58 +1,113 @@
         $(function () {            
-
-           var mySwiper1 = new Swiper ('.gallery1', {
-             loop: true, //순환유무	 
-             pagination: '.nav1', //네비게이션class명
-             //grabCursor: true, //커서 손모양
-             autoplay: 4000, //자동진행
-
-             //이전 다음 버튼
-             nextButton: '.swiper-button-next',
-             prevButton: '.swiper-button-prev',
-           })
-        
-            var mySwiper2 = new Swiper ('.gallery2', {
-             loop: true, //순환유무	 
-             pagination: '.nav2', //네비게이션class명
-            /* grabCursor: true, //커서 손모양*/
-             autoplay: 5000, //자동진행
-             effect: 'slide', // 'slide' or 'fade' or 'cube' or 'coverflow' or 'flip'
-
-             //이전 다음 버튼
-             nextButton: '.swiper-button-next2',
-             prevButton: '.swiper-button-prev2',
-           })
-           
-            var mySwiper3 = new Swiper ('.gallery3', {
-             loop: true, //순환유무	 
-             pagination: '.nav3', //네비게이션class명
-            /* grabCursor: true, //커서 손모양*/
-             autoplay: 6000, //자동진행
-             effect: 'slide', // 'slide' or 'fade' or 'cube' or 'coverflow' or 'flip'
-
-             //이전 다음 버튼
-             nextButton: '.swiper-button-next3',
-             prevButton: '.swiper-button-prev3',
-           })
-           
-           var mySwiper4 = new Swiper ('.gallery4', {
-             loop: true, //순환유무	 
-             pagination: '.nav3', //네비게이션class명
-            /* grabCursor: true, //커서 손모양*/
-             autoplay: 7000, //자동진행
-             effect: 'slide', // 'slide' or 'fade' or 'cube' or 'coverflow' or 'flip'
-
-             //이전 다음 버튼
-             nextButton: '.swiper-button-next4',
-             prevButton: '.swiper-button-prev4',
-           })
+          var mySwiper1 = new Swiper('.swiper-container1', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            speed: 500,
+            autoplay: {
+                delay: 5000,
+            },
+    
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+    
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+    
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+            })
+    
+            var mySwiper2 = new Swiper('.swiper-container2', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            speed: 500,
+            autoplay: {
+                delay: 3000,
+            },
+    
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+    
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+    
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+            })
+    
+            var mySwiper3 = new Swiper('.swiper-container3', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            speed: 500,
+            autoplay: {
+                delay: 4000,
+            },
+    
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+    
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+    
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+            })
+    
+            var mySwiper4 = new Swiper('.swiper-container4', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            speed: 500,
+            autoplay: {
+                delay: 6000,
+            },
+    
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+    
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+    
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+            })
            
            
            //미리보기                       
-           $('.items li').mouseenter(function(){
+           $('.items li').on("mouseenter",function(){
                 $(this).children('.preview').stop().slideToggle(150);
-           });           
-           $('.items li').mouseleave(function(){
+           }).on("mouseleave", function(){
                 $(this).children('.preview').stop().slideToggle(500);
            });
            
@@ -71,23 +126,23 @@
                     }                   
             });
 
-            gnbA.mouseleave(function(){                                       
-                        $(this).children().children().hide();                
+            gnbA.on("mouseleave",function(){                                       
+              $(this).children().children().hide();                
             });
                     
-            down_list.hover(function(){
+            down_list.on("mouseenter",function(){
                 $(this).show();
             },function(){
                 $(this).hide();
             });
            
            //주메뉴 호버css            
-           gnbA.hover(function(){
+           gnbA.on("mouseenter",function(){
                 $(this).children().css({color:'darkred'});
-           },function(){
+           }).on("mouseleave",function(){
                 $(this).children().css({color:'#333'});
            });
-           
+
           $('#gnb>.gnb_list>.list>li').hover(function(){
                 $(this).css({background:'#eee'});            
           });
